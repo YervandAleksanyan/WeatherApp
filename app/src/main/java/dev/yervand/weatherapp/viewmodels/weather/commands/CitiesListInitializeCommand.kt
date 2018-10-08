f274f716490d400813487d73b122243a2c5f63c2
YervandAlexanyan <yervandalexanyan@gmail.com>
2018-10-08 19:15:44 +0400

    CitiesViewModel moved to weather viewmodel package

--- app/src/main/java/dev/yervand/weatherapp/viewmodels/weather/commands/CitiesListInitializeCommand.kt
+++ app/src/main/java/dev/yervand/weatherapp/viewmodels/weather/commands/CitiesListInitializeCommand.kt
@@ -1,7 +1,7 @@
 package dev.yervand.weatherapp.viewmodels.weather.commands
 
-import dev.yervand.weatherapp.utils.CitiesViewModel
 import dev.yervand.weatherapp.viewmodels.base.implementation.BaseCommand
+import dev.yervand.weatherapp.viewmodels.weather.CitiesViewModel
 import dev.yervand.weatherapp.viewmodels.weather.WeatherViewModel
 
 class CitiesListInitializeCommand(private val viewModel: WeatherViewModel) : BaseCommand() {
